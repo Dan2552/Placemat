e510da15f510221ff1697c8927f84ca29efb32c4
Daniel Green <dan2552@gmail.com>
2016-11-09 20:13:23 +0000

    Navigation - adds modalDismissType argument to show

--- Placemat/Classes/Navigation.swift
+++ Placemat/Classes/Navigation.swift
@@ -4,9 +4,7 @@ enum NavigationError: Error {
     case WindowAlreadyExists
 }
 
-/**
- Helper class to navigate between view controllers.
- */
+/// Helper class to navigate between view controllers.
 public class Navigation {
     var currentViewController: UIViewController
     var animated: Bool
@@ -16,39 +14,41 @@ public class Navigation {
         self.animated = animated
     }
     
-    /**
-     Navigate to another controller.
-     
-     optional:
-     - **modally** - Defaults to false. If set to true, will embed the controller into a UINavigationController, and present that outside of the current navigation stack.
-     */
-    public func show(target: UIViewController, modally: Bool = false) {
-        if modally {
-            let nav = UINavigationController(rootViewController: target)
-            
-            let dismissCompletion: (() -> Void) = {
-                target.navigationItem.leftBarButtonItem = BlockBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.cancel) {
-                    Navigation(viewController: target).dismiss()
-                }
-            }
-            
-            currentViewController.present(nav,
-                                          animated: animated,
-                                          completion: dismissCompletion)
+    /// Navigate to another controller. See also `show(target:modally:modalDismissButtonSystemItem:)`
+    ///
+    /// - parameter target: The view controller to present.
+    public func show(target: UIViewController) {
+        if let navigation = currentViewController.navigationController {
+            navigation.pushViewController(target, animated: animated)
         } else {
-            if let navigation = currentViewController.navigationController {
-                navigation.pushViewController(target, animated: animated)
-            } else {
-                currentViewController.present(target,
-                                              animated: animated,
-                                              completion: nil)
-            }
+            currentViewController.present(target,
+                                          animated: animated,
+                                          completion: nil)
         }
     }
+    
+    /// Navigate to another controller.
+    ///
+    /// - parameter target: The view controller to present.
+    /// - parameter modally: If set to true, will embed the controller into a UINavigationController, and present that outside of the current navigation stack.
+    /// - parameter modalDismissType: Defaults to `.cancel`. Type of button used in the left bar button when showing modally.
+    public func show(target: UIViewController, modally: Bool, modalDismissType: UIBarButtonSystemItem = .cancel) {
+        guard modally else { return show(target: target) }
+        
+        let nav = UINavigationController(rootViewController: target)
+        
+        let dismissCompletion: (() -> Void) = {
+            target.navigationItem.leftBarButtonItem = BlockBarButtonItem(barButtonSystemItem: modalDismissType) {
+                Navigation(viewController: target).dismiss()
+            }
+        }
+        
+        currentViewController.present(nav,
+                                      animated: animated,
+                                      completion: dismissCompletion)
+    }
 
-    /**
-     Dismisses the view controller. Automatically determines how the controller should be dismissed appropriately (e.g. if it's shown modally or not)
-     */
+    /// Dismisses the view controller. Automatically determines how the controller should be dismissed appropriately (e.g. if it's shown modally or not)
     public func dismiss() {
         if let navigation = currentViewController.navigationController {
             if navigation.viewControllers.count == 1 {
@@ -61,13 +61,9 @@ public class Navigation {
         }
     }
     
-    /**
-     Used to setup a UIWindow containing the view controller.
-     
-     Normally this should be used within `application:didFinishLaunchingWithOptions:`.
-     
-     Make sure you keep a strong reference of the window.
-     */
+    /// Used to setup a UIWindow containing the view controller. Normally this should be used within `application:didFinishLaunchingWithOptions:`.
+    ///
+    /// Make sure you keep a strong reference of the window.
     public func setupWindow() throws -> UIWindow {
         guard currentViewController.view.window == nil else { throw NavigationError.WindowAlreadyExists }
         
